58f78c5d3b82919bd111d60997942179b24417c7
Deivi-0904 <santyrodrig19@gmail.com>
2023-09-10 12:14:03 -0500

    git comit

---
+++ app.js
@@ -0,0 +1,5 @@
+const functions=require('./functions.js')
+console.log(functions.add(5,3));
+console.log(functions.substract(2,5));
+console.log(functions.multiply(2,5));
+console.log(functions.divide(2,0.3));
